e7f3568f4c6017b157ab7a1c317b7cea072bb72f
Rishi Raj <86799086+RishiRaj11@users.noreply.github.com>
2021-09-24 15:25:44 +0530

    live code

--- Hello.java
+++ Hello.java
@@ -1,14 +1,48 @@
-import java.util.Scanner;
+/*
 
-/**
- * Hello
- */
-public class Hello {
+Have the function treeConstructor(strArr) take the array of strings stored in strArr, which will contain pairs of integers in the following format: (i1,i2), where i1 represents a child node in a tree and the second integer i2 signifies that it is the parent of i1.
 
-    public static void main(String[] args) {
+For example: if strArr is ["(1,2)", "(2,4)", "(7,2)"], then this forms the following tree:
+*/
+import java.lang.*;
+import java.util.*;
+
+class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        String [] strArr=new String[size];
+        for(int i=0;i<size;i++){
+            strArr[i]=sc.next();
+           // System.out.println(strArr[i].charAt(1));
+
+        }
+         System.out.println(treeConstructor(strArr));
+         
         
-        Scanner sc =new Scanner(System.in);
-        String x=sc.next();
-        System.out.println("Hello world " +x);
+    }
+
+    public static boolean  treeConstructor(String[] strArr){
+        boolean temp=false;
+        HashSet<Character> set=new HashSet<Character>();
+       for(int i=0;i<strArr.length;i++){
+           String str=strArr[i];
+          // for(int j=0;j<str.length();i++){
+               set.add(str.charAt(1));
+              
+          // }
+
+           
+       }
+       if(set.size()==strArr.length){
+            return true;
+       }else{
+       false;
+       }
+
+   
+
     }
 }
