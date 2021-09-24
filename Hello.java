b741921ab145a702bf2a1f7c1e5bb044a38a9c72
Rishi Raj <86799086+RishiRaj11@users.noreply.github.com>
2021-09-24 08:30:14 +0530

    Hello world

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+        String x=sc.next();
+        System.out.println("Hello world " +x);
+    }
+}
